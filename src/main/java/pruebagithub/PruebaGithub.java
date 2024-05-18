9075430814ef394e8398dd3a43c6e101200eb5ff
AliciaSnchezP <snchez.alicia1@gmail.com>
2024-05-18 15:11:37 +0200

    Commit inicial App de JAVA

---
+++ src/main/java/pruebagithub/PruebaGithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pruebagithub;
+
+/**
+ *
+ * @author Alicia
+ */
+public class PruebaGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
